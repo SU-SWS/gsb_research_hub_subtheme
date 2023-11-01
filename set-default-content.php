de4ec4c1341f0b5bab79f78e6f16925f5c08ec60
Adam Moore <redndahead@gmail.com>
2023-11-01 15:46:35 -0700

    Add support for displaying trainings.

--- set-default-content.php
+++ set-default-content.php
@@ -1,60 +1,89 @@
 <?php
-/*********************
- ** Service Catalog **
- *********************/
 
-// Create Service Catalog Node
-$sc_node = \Drupal::entityTypeManager()->getStorage("node")->create([
-  "type" => "stanford_page",
-  "title" => "Service Catalog"
-]);
-$sc_node->save();
+/****************
+ ** Menu Items **
+ ****************/
+// External Link
+\Drupal\menu_link_content\Entity\MenuLinkContent::create(["title" => "Submenu link", "link" => ["uri" => "https://google.com"], "menu_name" => "main", "parent" => "menu_link_field:node_field_menulink_e18a741f-7d86-492a-a02f-7ca73989ca13_und", "expanded" => TRUE, "weight" => 0])->save();
 
-// Create layout for paragraph for Service Catalog.
-$sc_paragraph_layout = \Drupal::entityTypeManager()->getStorage("paragraph")->create([
-  "type" => "stanford_layout",
-  "parent_id" => $sc_node->id(),
-  "parent_type" => "node",
-  "parent_field_name" => "su_page_components"
-]);
 
-$sc_paragraph_layout->setBehaviorSettings("layout_paragraphs", [
-  "layout" => "layout_paragraphs_1_column",
-  "config" => [
-    "label" => ""
-  ],
-  "parent_uuid" => "",
-  "region" => ""
-]);
-$sc_paragraph_layout->save();
+/********************
+ ** Airtable Items **
+ ********************/
 
-// Create paragraph for Service Catalog
-$sc_paragraph = \Drupal::entityTypeManager()->getStorage("paragraph")->create([
-  "type" => "stanford_wysiwyg",
-  "parent_id" => $sc_node->id(),
-  "parent_type" => "node",
-  "parent_field_name" => "su_page_components",
-  "su_wysiwyg_text" => '{"type":"airtable","view": "service_catalog"}'
-]);
-
-$sc_paragraph->setBehaviorSettings("layout_paragraphs", [
-  "parent_uuid" => $sc_paragraph_layout->uuid(),
-  "region" => "main"
-]);
-$sc_paragraph->save();
-
-// Add paragraph to service catalog node.
-$sc_node->su_page_components = [
+$airtable_pages = [
   [
-    "target_id" => $sc_paragraph_layout->id(),
-    "target_revision_id" => $sc_paragraph_layout->getRevisionId()
+    'title' => 'Service Catalog',
+    'wysiwyg_text' => '{"type":"airtable","view": "service_catalog"}',
   ],
   [
-    "target_id" => $sc_paragraph->id(),
-    "target_revision_id" => $sc_paragraph->getRevisionId()
-  ]
+    'title' => 'Past Trainings & Workshops',
+    'wysiwyg_text' => '{"type":"airtable","view": "training_past"}',
+  ],
+  [
+    'title' => 'Upcoming Trainings & Workshops',
+    'wysiwyg_text' => '{"type":"airtable","view": "training_upcoming"}',
+  ],
 ];
-$sc_node->save();
+
+
+foreach ($airtable_pages as $page) {
+  // Create Service Catalog Node
+  $at_node = \Drupal::entityTypeManager()->getStorage("node")->create([
+    "type" => "stanford_page",
+    "title" => $page['title'],
+  ]);
+  $at_node->save();
+  
+  // Create layout for paragraph for Service Catalog.
+  $at_paragraph_layout = \Drupal::entityTypeManager()->getStorage("paragraph")->create([
+    "type" => "stanford_layout",
+    "parent_id" => $at_node->id(),
+    "parent_type" => "node",
+    "parent_field_name" => "su_page_components",
+  ]);
+  
+  $at_paragraph_layout->setBehaviorSettings("layout_paragraphs", [
+    "layout" => "layout_paragraphs_1_column",
+    "config" => [
+      "label" => ""
+    ],
+    "parent_uuid" => "",
+    "region" => ""
+  ]);
+  $at_paragraph_layout->save();
+  
+  // Create paragraph for Service Catalog
+  $at_paragraph = \Drupal::entityTypeManager()->getStorage("paragraph")->create([
+    "type" => "stanford_wysiwyg",
+    "parent_id" => $at_node->id(),
+    "parent_type" => "node",
+    "parent_field_name" => "su_page_components",
+    "su_wysiwyg_text" => $page['wysiwyg_text'],
+  ]);
+  
+  $at_paragraph->setBehaviorSettings("layout_paragraphs", [
+    "parent_uuid" => $at_paragraph_layout->uuid(),
+    "region" => "main"
+  ]);
+  $at_paragraph->save();
+  
+  // Add paragraph to service catalog node.
+  $at_node->su_page_components = [
+    [
+      "target_id" => $at_paragraph_layout->id(),
+      "target_revision_id" => $at_paragraph_layout->getRevisionId()
+    ],
+    [
+      "target_id" => $at_paragraph->id(),
+      "target_revision_id" => $at_paragraph->getRevisionId()
+    ]
+  ];
+  $at_node->save();
+
+  // Set Menu
+  \Drupal\menu_link_content\Entity\MenuLinkContent::create(["title" => $page['title'], "link" => ["uri" => "internal:/node/" . $at_node->id()], "menu_name" => "main", "parent" => "menu_link_field:node_field_menulink_e18a741f-7d86-492a-a02f-7ca73989ca13_und", "expanded" => TRUE, "weight" => 0])->save();
+}
 
 
 /****************

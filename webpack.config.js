cc6002fcd12d45e89006cbaf27bdc09d33f48317
Jen Breese-Kauth <jbreese@stanford.edu>
2024-02-20 19:39:33 +0000

    updating webpack and node

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
-const config = require("./src/config");
+
+const path = require("path");
 const Webpack = require("webpack");
-const AssetsWebpackPlugin = require('assets-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const autoprefixer = require('autoprefixer')({ grid: true });
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+const config = {
+  isProd: process.env.NODE_ENV === "production",
+  hmrEnabled: process.env.NODE_ENV !== "production" && !process.env.NO_HMR,
+  distFolder: path.resolve(__dirname, "./dist/css"),
+  wdsPort: 3001,
+};
 
 var webpackConfig = {
   entry: {
-    "css/main": ["./src/scss/main.scss"],
-    "css/ckeditor5": ["./src/scss/ckeditor5.scss"],
+    "main": ["./src/scss/main.scss"],
+    "ckeditor5": ["./src/scss/ckeditor5.scss"],
     "js/airtable-list-builder": "./src/js/airtable-list-builder.js"
   },
   output: {
     path: config.distFolder,
     filename: '[name].js',
-    publicPath: config.publicPath,
-    clean: true
+    assetModuleFilename: '../assets/[name][ext][query]'
   },
   mode: config.isProd ? "production" : "development",
+  resolve: {
+    alias: {
+      'decanter-assets': path.resolve('node_modules', 'decanter/core/src/img'),
+      'decanter-src': path.resolve('node_modules', 'decanter/core/src'),
+      '@fortawesome': path.resolve('node_modules', '@fortawesome'),
+      'fa-fonts': path.resolve('node_modules', '@fortawesome/fontawesome-free/webfonts')
+    }
+  },
   module: {
     rules: [
       {
@@ -33,17 +50,36 @@ var webpackConfig = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           config.isProd ? { loader: MiniCssExtractPlugin.loader } : 'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
-        ],
+          {loader:'css-loader', options: {}},
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                sourceMap: true,
+                plugins: [autoprefixer],
+              },
+            }
+          },
+          {loader:'sass-loader', options: {}}
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset"
       }
     ]
   },
   plugins: [
-    new AssetsWebpackPlugin({path: config.distFolder}),
+    new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].css',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ["dist"]
+        }
+      }
     }),
   ],
   optimization: {
@@ -51,7 +87,6 @@ var webpackConfig = {
       new OptimizeCSSAssetsPlugin(),
     ]
   }
-
 };
 
 if (config.hmrEnabled) {

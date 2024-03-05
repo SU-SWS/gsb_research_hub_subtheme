3176d742a0c503b135858b96f34dd5e644a79742
Adam Moore <redndahead@gmail.com>
2024-03-05 05:28:15 +0000

    RHW-54: Remove extraneous question mark when there are no parameters in airtable.js.

--- src/js/airtable-list-builder.js
+++ src/js/airtable-list-builder.js
@@ -254,7 +254,15 @@
                 }
 
                 // Set the browser url.
-                window.history.replaceState(null, null,'?' + urlParamaters.toString());
+                let params = urlParamaters.toString();
+                // If there are no parameters set then reset the url to no parameters.
+                // Otherwise set it to the given filters.
+                if (params === '') {
+                  window.history.replaceState(null, null, window.location.pathname);
+                }
+                else {
+                  window.history.replaceState(null, null,'?' + params);
+                }
 
                 // combine filters
                 let filterValue = buildFilters(filters);

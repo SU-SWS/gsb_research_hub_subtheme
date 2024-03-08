d5d07aca8480bbe35d37e71b2c675dac576c61a6
Adam Moore <redndahead@gmail.com>
2024-03-07 17:55:07 -0800

    Fix typos throwing an error on build.

--- set-default-content.php
+++ set-default-content.php
@@ -86,9 +86,9 @@ foreach ($pages as $page) {
       "target_revision_id" => $paragraph->getRevisionId()
     ]
   ];
-  $at_node->save();
+  $node->save();
 
   // Set Menu
   $menu = \Drupal\menu_link_content\Entity\MenuLinkContent::create(["title" => $page['title'], "link" => ["uri" => "internal:/node/" . $node->id()], "menu_name" => "main", "parent" => $parent_menu_links[$page['menu_parent']], "expanded" => TRUE, "weight" => 0])->save();
-  $parent_menu_links[$page_title] = "menu_link_content:" . $at_menu->uuid();
+  $parent_menu_links[$page_title] = "menu_link_content:" . $menu->uuid();
 }
